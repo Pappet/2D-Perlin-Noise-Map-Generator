e4271c2ba7c3bd37166e93e2a37998c4e28594f1
Pappet <pappet@web.de>
2017-08-22 18:23:04 +0200

    MouseManager

---
+++ Small-Strategy-Game/Assets/MouseManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+	public GameObject SelectorPrefab;
+	public MapGenerator mapGenerator;
+	bool selected = false;
+	Tile tile;
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			if(!selected){
+				Vector2 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+				tile = mapGenerator.GetTileAt((int)pos.x,(int)pos.y);
+				pos.x = Mathf.Abs(pos.x);
+				pos.y = Mathf.Abs(pos.y);
+				Instantiate(SelectorPrefab, pos, Quaternion.identity);
+				selected = true;
+			}
+		}
+
+	}
+}
